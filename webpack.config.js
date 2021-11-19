a431ae2db3beb4f58c817f537fd88d840bb6097e
Esto Triramdani N <estolagi@gmail.com>
2021-11-19 09:11:24 +0700

    config webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.ts',
   module: {
     rules: [
@@ -14,5 +15,13 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js',
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 4000,
+    allowedHosts: 'all',
   },
 };
